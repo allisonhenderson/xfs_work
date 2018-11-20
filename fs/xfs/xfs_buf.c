909f3e5a3a0675a405e5a9c57d1aefcb3f41a7fa
Allison Henderson <allison.henderson@oracle.com>
2018-11-19 21:46:17 -0700

    xfs: Rewrite retried read
    If we had to try more than one mirror to get a successful
    read, then write that buffer back to correct the bad mirro
    
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>

--- fs/xfs/xfs_buf.c
+++ fs/xfs/xfs_buf.c
@@ -847,6 +847,14 @@ xfs_buf_read_map(
 
 		}
 retry_done:
+
+		/*
+		 * if we had to try more than one mirror to sucessfully read
+		 * the buffer, write the buffer back
+		 */
+		if (!bp->b_error && i > 0)
+			xfs_bwrite(bp);
+
 		return bp;
 	}
 
