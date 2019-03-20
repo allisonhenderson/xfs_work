8382917eff473cfe039fa88571e4659239054c7f
Allison Henderson <allison.henderson@oracle.com>
2019-03-19 19:31:10 -0700

    xfs: Hold inode locks in xfs_ialloc
    Modify xfs_ialloc to hold locks after return.  Caller will be
    responsible for manual unlock.  We will need this later to hold locks
    across parent pointer operations
    
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>

--- fs/xfs/xfs_symlink.c
+++ fs/xfs/xfs_symlink.c
@@ -328,6 +328,7 @@ xfs_symlink(
 	xfs_qm_dqrele(pdqp);
 
 	*ipp = ip;
+	xfs_iunlock(ip, XFS_ILOCK_EXCL);
 	return 0;
 
 out_trans_cancel:
@@ -349,6 +350,8 @@ out_release_inode:
 
 	if (unlock_dp_on_error)
 		xfs_iunlock(dp, XFS_ILOCK_EXCL);
+	if (ip)
+		xfs_iunlock(ip, XFS_ILOCK_EXCL);
 	return error;
 }
 
