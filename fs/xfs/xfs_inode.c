880bb03c54280b390c2303ad7a9c96a2464ca3c5
Allison Henderson <allison.henderson@oracle.com>
2019-03-20 20:07:52 -0700

    xfs: get directory offset when adding directory name
    Return the directory offset information when adding an entry to the directory.
    
    This offset will be used as the parent pointer offset in xfs_create,
    xfs_symlink, xfs_link and xfs_rename.
    
    [dchinner: forward ported and cleaned up]
    [dchinner: no s-o-b from Mark]
    [bfoster: rebased, use args->geo in dir code]
    [achender: rebased, chaged __uint32_t to xfs_dir2_dataptr_t]
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -1222,7 +1222,8 @@ xfs_create(
 
 	error = xfs_dir_createname(tp, dp, name, ip->i_ino,
 				   resblks ?
-					resblks - XFS_IALLOC_SPACE_RES(mp) : 0);
+					resblks - XFS_IALLOC_SPACE_RES(mp) : 0,
+					NULL);
 	if (error) {
 		ASSERT(error != -ENOSPC);
 		goto out_trans_cancel;
@@ -1448,7 +1449,7 @@ xfs_link(
 	}
 
 	error = xfs_dir_createname(tp, tdp, target_name, sip->i_ino,
-				   resblks);
+				   resblks, NULL);
 	if (error)
 		goto error_return;
 	xfs_trans_ichgtime(tp, tdp, XFS_ICHGTIME_MOD | XFS_ICHGTIME_CHG);
@@ -3358,7 +3359,7 @@ xfs_rename(
 		 * to account for the ".." reference from the new entry.
 		 */
 		error = xfs_dir_createname(tp, target_dp, target_name,
-					   src_ip->i_ino, spaceres);
+					   src_ip->i_ino, spaceres, NULL);
 		if (error)
 			goto out_trans_cancel;
 
