9e8e0a77220d6acc1c25cea6ca015decb77a76f1
Allison Henderson <allison.henderson@oracle.com>
2019-03-20 20:40:49 -0700

    xfs: Add parent pointer ioctl
    This patch adds a new file ioctl to retrieve the parent
    pointer of a given inode
    
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>

--- fs/xfs/xfs_parent_utils.c
+++ fs/xfs/xfs_parent_utils.c
@@ -30,6 +30,7 @@
 #include "xfs_da_btree.h"
 #include "xfs_attr.h"
 #include "xfs_parent.h"
+#include "xfs_da_btree.h"
 
 /*
  * Add a parent record to an inode with existing parent records.
@@ -66,5 +67,88 @@ xfs_parent_remove_deferred(
 
 	return xfs_attr_remove_deferred(child, tp, (const char *)&rec,
 		sizeof(rec), ATTR_PARENT);
+}
+
+/*
+ * Get the parent pointers for a given inode
+ *
+ * Returns 0 on success and non zero on error
+ */
+int
+xfs_attr_get_parent_pointer(struct xfs_inode		*ip,
+			    struct xfs_pptr_info	*ppi)
+
+{
+
+	struct attrlist			*alist;
+	struct attrlist_ent		*aent;
+	struct xfs_parent_ptr		*xpp;
+	struct xfs_parent_name_rec	*xpnr;
+	char				*namebuf;
+	unsigned int			namebuf_size;
+	int				name_len;
+	int				error = 0;
+	unsigned int			flags = ATTR_PARENT;
+	int				i;
+	struct xfs_attr_list_context	context;
+	struct xfs_da_args		args;
+
+	/* Allocate a buffer to store the attribute names */
+	namebuf_size = sizeof(struct attrlist) +
+		       (ppi->pi_ptrs_size) * sizeof(struct attrlist_ent);
+	namebuf = kmem_zalloc_large(namebuf_size, KM_SLEEP);
+	if (!namebuf)
+		return -ENOMEM;
+
+	error = xfs_attr_list_context_init(ip, namebuf, namebuf_size, flags,
+			(attrlist_cursor_kern_t *)&ppi->pi_cursor, &context);
+	if (error)
+		goto out_kfree;
+
+	xfs_ilock(ip, XFS_ILOCK_EXCL);
+
+	error = xfs_attr_list_int_ilocked(&context);
+	if (error)
+		goto out_kfree;
+
+	alist = (struct attrlist *)namebuf;
+	for (i = 0; i < alist->al_count; i++) {
+		xpp = XFS_PPINFO_TO_PP(ppi, i);
+		memset(xpp, 0, sizeof(struct xfs_parent_ptr));
+		aent = (struct attrlist_ent *) &namebuf[alist->al_offset[i]];
+		xpnr = (struct xfs_parent_name_rec *)(aent->a_name);
+
+		if (aent->a_valuelen > XFS_PPTR_MAXNAMELEN) {
+			error = -ERANGE;
+			goto out_kfree;
+		}
+		name_len = aent->a_valuelen;
+
+		error = xfs_attr_args_init(&args, ip, (char *)xpnr,
+				sizeof(struct xfs_parent_name_rec), flags);
+		if (error)
+			goto out_kfree;
+
+		args.value = (unsigned char *)(xpp->xpp_name);
+		args.valuelen = name_len;
+		args.op_flags = XFS_DA_OP_OKNOENT;
+
+		error = xfs_attr_get_ilocked(ip, &args);
+		error = (error == -EEXIST ? 0 : error);
+		if (error)
+			goto out_kfree;
+
+		xpp->xpp_namelen = name_len;
+		xfs_init_parent_ptr(xpp, xpnr);
+	}
+	ppi->pi_ptrs_used = alist->al_count;
+	if (!alist->al_more)
+		ppi->pi_flags |= XFS_PPTR_OFLAG_DONE;
+
+out_kfree:
+	xfs_iunlock(ip, XFS_ILOCK_EXCL);
+	kmem_free(namebuf);
+
+	return error;
 }
 
