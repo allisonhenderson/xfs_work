8bc7332c6d29640ba6da78873b2d48ea9a26598c
Allison Henderson <allison.henderson@oracle.com>
2019-03-20 20:10:04 -0700

    xfs: get directory offset when replacing a directory name
    Return the directory offset information when replacing an entry to the
    directory.
    
    This offset will be used as the parent pointer offset in xfs_rename.
    
    [dchinner: forward ported and cleaned up]
    [achender: rebased, changed __unint32_t to xfs_dir2_dataptr_t,
               Changed typedefs to raw struct types]
    
    Signed-off-by: Mark Tinguely <tinguely@sgi.com>
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/libxfs/xfs_dir2.c
+++ fs/xfs/libxfs/xfs_dir2.c
@@ -470,7 +470,7 @@ xfs_dir_removename(
 	else
 		rval = xfs_dir2_node_removename(args);
 out_free:
-	if (offset)
+	if (!rval && offset)
 		*offset = args->offset;
 
 	kmem_free(args);
@@ -486,7 +486,8 @@ xfs_dir_replace(
 	struct xfs_inode	*dp,
 	struct xfs_name		*name,		/* name of entry to replace */
 	xfs_ino_t		inum,		/* new inode number */
-	xfs_extlen_t		total)		/* bmap's total block count */
+	xfs_extlen_t		total,		/* bmap's total block count */
+	xfs_dir2_dataptr_t	*offset)	/* OUT: offset in directory */
 {
 	struct xfs_da_args	*args;
 	int			rval;
@@ -534,6 +535,9 @@ xfs_dir_replace(
 	else
 		rval = xfs_dir2_node_replace(args);
 out_free:
+	if (offset)
+		*offset = args->offset;
+
 	kmem_free(args);
 	return rval;
 }
