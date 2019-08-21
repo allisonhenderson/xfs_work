be6716add6114fb60e6dbe9c319d66ae3670dfed
Allison Collins <allison.henderson@oracle.com>
2019-08-21 15:58:23 -0700

    xfs: Add feature bit XFS_SB_FEAT_INCOMPAT_LOG_DELATTR
    This patch adds a new feature bit XFS_SB_FEAT_INCOMPAT_LOG_DELATTR which
    can be used to control turning on/off delayed attributes
    
    Signed-off-by: Allison Collins <allison.henderson@oracle.com>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1542,6 +1542,10 @@ xfs_fc_fill_super(
 		goto out_filestream_unmount;
 	}
 
+	if (xfs_sb_version_hasdelattr(&mp->m_sb))
+		xfs_alert(mp,
+	"EXPERIMENTAL delayed attrs feature enabled. Use at your own risk!");
+
 	error = xfs_mountfs(mp);
 	if (error)
 		goto out_filestream_unmount;
