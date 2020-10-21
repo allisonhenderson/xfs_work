29a779c918f46e2749fe4da101239a06a12b6007
Allison Henderson <allison.henderson@oracle.com>
2020-10-21 11:23:46 -0700

    xfs: Add the parent pointer support to the  superblock version 5.
    [dchinner: forward ported and cleaned up]
    [achender: rebased and added parent pointer attribute to
               compatible attributes mask]
    
    Signed-off-by: Mark Tinguely <tinguely@sgi.com>
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/libxfs/xfs_sb.c
+++ fs/xfs/libxfs/xfs_sb.c
@@ -1170,6 +1170,8 @@ xfs_fs_geometry(
 		geo->flags |= XFS_FSOP_GEOM_FLAGS_BIGTIME;
 	if (xfs_sb_version_hasdelattr(sbp))
 		geo->flags |= XFS_FSOP_GEOM_FLAGS_DELATTR;
+	if(xfs_sb_version_hasparent(sbp))
+		geo->flags |= XFS_FSOP_GEOM_FLAGS_PARENT;
 	if (xfs_sb_version_hassector(sbp))
 		geo->logsectsize = sbp->sb_logsectsize;
 	else
