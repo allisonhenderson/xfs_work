2c9a3da892d56097cb8f6a6a2d250ac3e43501f6
Allison Henderson <allison.henderson@oracle.com>
2020-11-20 19:45:04 -0700

    xfsprogs: Add delay ready attr remove routines
    This patch modifies the attr remove routines to be delay ready. This
    means they no longer roll or commit transactions, but instead return
    -EAGAIN to have the calling routine roll and refresh the transaction. In
    this series, xfs_attr_remove_args has become xfs_attr_remove_iter, which
    uses a sort of state machine like switch to keep track of where it was
    when EAGAIN was returned. xfs_attr_node_removename has also been
    modified to use the switch, and a new version of xfs_attr_remove_args
    consists of a simple loop to refresh the transaction until the operation
    is completed. A new XFS_DAC_DEFER_FINISH flag is used to finish the
    transaction where ever the existing code used to.
    
    Calls to xfs_attr_rmtval_remove are replaced with the delay ready
    version __xfs_attr_rmtval_remove. We will rename
    __xfs_attr_rmtval_remove back to xfs_attr_rmtval_remove when we are
    done.
    
    xfs_attr_rmtval_remove itself is still in use by the set routines (used
    during a rename).  For reasons of preserving existing function, we
    modify xfs_attr_rmtval_remove to call xfs_defer_finish when the flag is
    set.  Similar to how xfs_attr_remove_args does here.  Once we transition
    the set routines to be delay ready, xfs_attr_rmtval_remove is no longer
    used and will be removed.
    
    This patch also adds a new struct xfs_delattr_context, which we will use
    to keep track of the current state of an attribute operation. The new
    xfs_delattr_state enum is used to track various operations that are in
    progress so that we know not to repeat them, and resume where we left
    off before EAGAIN was returned to cycle out the transaction. Other
    members take the place of local variables that need to retain their
    values across multiple function recalls.  See xfs_attr.h for a more
    detailed diagram of the states.
    
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>

--- libxfs/xfs_attr_remote.c
+++ libxfs/xfs_attr_remote.c
@@ -675,10 +675,12 @@ xfs_attr_rmtval_invalidate(
  */
 int
 xfs_attr_rmtval_remove(
-	struct xfs_da_args      *args)
+	struct xfs_da_args		*args)
 {
-	int			error;
-	int			retval;
+	int				error;
+	struct xfs_delattr_context	dac  = {
+		.da_args	= args,
+	};
 
 	trace_xfs_attr_rmtval_remove(args);
 
@@ -686,19 +688,16 @@ xfs_attr_rmtval_remove(
 	 * Keep de-allocating extents until the remote-value region is gone.
 	 */
 	do {
-		retval = __xfs_attr_rmtval_remove(args);
-		if (retval && retval != -EAGAIN)
-			return retval;
+		error = __xfs_attr_rmtval_remove(&dac);
+		if (error != -EAGAIN)
+			break;
 
-		/*
-		 * Close out trans and start the next one in the chain.
-		 */
-		error = xfs_trans_roll_inode(&args->trans, args->dp);
+		error = xfs_attr_trans_roll(&dac);
 		if (error)
 			return error;
-	} while (retval == -EAGAIN);
+	} while (true);
 
-	return 0;
+	return error;
 }
 
 /*
@@ -708,9 +707,10 @@ xfs_attr_rmtval_remove(
  */
 int
 __xfs_attr_rmtval_remove(
-	struct xfs_da_args	*args)
+	struct xfs_delattr_context	*dac)
 {
-	int			error, done;
+	struct xfs_da_args		*args = dac->da_args;
+	int				error, done;
 
 	/*
 	 * Unmap value blocks for this attr.
@@ -720,12 +720,10 @@ __xfs_attr_rmtval_remove(
 	if (error)
 		return error;
 
-	error = xfs_defer_finish(&args->trans);
-	if (error)
-		return error;
-
-	if (!done)
+	if (!done) {
+		dac->flags |= XFS_DAC_DEFER_FINISH;
 		return -EAGAIN;
+	}
 
 	return error;
 }
