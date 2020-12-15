b43c534a1faea8a1b384d842509896a11dae5f66
Allison Henderson <allison.henderson@oracle.com>
2020-12-14 23:47:38 -0700

    xfsprogs: Rename __xfs_attr_rmtval_remove
    Now that xfs_attr_rmtval_remove is gone, rename __xfs_attr_rmtval_remove
    to xfs_attr_rmtval_remove
    
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>

--- libxfs/xfs_attr.c
+++ libxfs/xfs_attr.c
@@ -887,7 +887,7 @@ das_flip_flag:
 	dac->dela_state = XFS_DAS_RM_LBLK;
 das_rm_lblk:
 	if (args->rmtblkno) {
-		error = __xfs_attr_rmtval_remove(dac);
+		error = xfs_attr_rmtval_remove(dac);
 		if (error == -EAGAIN)
 			trace_xfs_das_state_return(dac->dela_state);
 		if (error)
@@ -1255,7 +1255,7 @@ das_flip_flag:
 	dac->dela_state = XFS_DAS_RM_NBLK;
 das_rm_nblk:
 	if (args->rmtblkno) {
-		error = __xfs_attr_rmtval_remove(dac);
+		error = xfs_attr_rmtval_remove(dac);
 
 		if (error == -EAGAIN)
 			trace_xfs_das_state_return(dac->dela_state);
@@ -1402,7 +1402,7 @@ xfs_attr_node_remove_rmt (
 	/*
 	 * May return -EAGAIN to request that the caller recall this function
 	 */
-	error = __xfs_attr_rmtval_remove(dac);
+	error = xfs_attr_rmtval_remove(dac);
 	if (error == -EAGAIN)
 		trace_xfs_das_state_return(dac->dela_state);
 	if (error)
