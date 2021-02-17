6227040445832c531c5946270a63c2342af0aa55
Allison Henderson <allison.henderson@oracle.com>
2021-02-17 10:02:08 -0700

    xfs: get directory offset when removing directory name
    Return the directory offset information when removing an entry to the
    directory.
    
    This offset will be used as the parent pointer offset in xfs_remove.
    
    [dchinner: forward ported and cleaned up]
    [achender: rebased, changed __unint32_t to xfs_dir2_dataptr_t,
               Changed typedefs to raw struct types]
    
    Signed-off-by: Mark Tinguely <tinguely@sgi.com>
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>

--- fs/xfs/xfs_inode.c
+++ fs/xfs/xfs_inode.c
@@ -2813,7 +2813,7 @@ xfs_remove(
 	if (error)
 		goto out_trans_cancel;
 
-	error = xfs_dir_removename(tp, dp, name, ip->i_ino, resblks);
+	error = xfs_dir_removename(tp, dp, name, ip->i_ino, resblks, NULL);
 	if (error) {
 		ASSERT(error != -ENOENT);
 		goto out_trans_cancel;
@@ -3375,7 +3375,7 @@ xfs_rename(
 		 * place if the extent count would overflow.
 		 */
 		error = xfs_dir_removename(tp, src_dp, src_name, src_ip->i_ino,
-					   spaceres);
+					   spaceres, NULL);
 	}
 
 	if (error)
